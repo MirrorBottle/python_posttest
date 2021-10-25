acba8ee9ee8dd7cf83239e2c8fb561228244fa41
MirrorBottle <setiawanbayu66152@gmail.com>
2021-10-25 16:36:57 +0800

    feat: done module_one

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python Posttest Repo
--- posttest_two/module_one.py
+++ posttest_two/module_one.py
@@ -1,1 +1,40 @@
-print('something')
+import math
+choice = "0"
+
+def choices():
+  print("""
+KONVERSI SUHU
+Pilih konversi di bawah ini:
+1. Fahrenheit ke Celsius
+2. Kelvin ke Celsius
+3. Reamur ke Celsius
+4. Keluar
+  """)
+  choice = input("Pilih Konversi : ")
+  return choice
+
+while choice != "4":
+  choice = choices()
+  if choice == "1":
+    fahrenheit = float(input("Masukkan Nilai Fahrenheit : "))
+    celcius = math.floor(fahrenheit - 32 / 1.8)
+    print(f"Hasil : {fahrenheit} F > {celcius} C")
+
+  elif choice == "2":
+
+    kelvin = float(input("Masukkan Nilai Kelvin : "))
+    celcius = math.floor(kelvin - 273.15)
+    print(f"Hasil : {kelvin} K > {celcius} C")
+
+  elif choice == "3":
+
+    reamur = float(input("Masukkan Nilai Reamur : "))
+    celcius = math.floor(reamur / 0.8)
+    print(f"Hasil : {reamur} R > {celcius} C")
+
+  choice = choices()
+
+print("Terima kasih!")
+   
+  
+  
